08539535b70249296da4934b7f5845166dc6df3b
DESKTOP-SG7TUT9\ryan1 <ryan1115@live.hk>
2024-02-12 04:34:45 +0800

    add md5 in login

--- sky-pojo/src/main/java/com/sky/dto/EmployeeLoginDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeeLoginDTO.java
@@ -7,13 +7,13 @@ import lombok.Data;
 import java.io.Serializable;
 
 @Data
-@ApiModel(description = "员工登录时传递的数据模型")
+@ApiModel(description = "員工登錄時傳遞的數據模型")
 public class EmployeeLoginDTO implements Serializable {
 
-    @ApiModelProperty("用户名")
+    @ApiModelProperty(value = "用戶名")
     private String username;
 
-    @ApiModelProperty("密码")
+    @ApiModelProperty(value = "密碼")
     private String password;
 
 }
